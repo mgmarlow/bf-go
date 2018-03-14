718e8737f1741fea0e44c8a69bf2fe9265bd050e
Graham Marlow <mgmarlow@gmail.com>
2018-03-13 21:14:03 -0700

    add example code

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"os"
+
 	"github.com/mgmarlow/brainfuck/brain"
 )
 
 func main() {
-	test := []byte("++++++ [ > ++++++++++ < - ] > +++++ .")
+	bfFile, err := ioutil.ReadFile("./hello.bf")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "reading input file: %v", err)
+	}
+
 	vm := &brain.BrainVM{}
-	vm.Interpret(test)
+	vm.Interpret(bfFile)
 }
